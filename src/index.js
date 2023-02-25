5bac785469badfaed74cb8f8ba5c80ceeb6c32a7
IndiCasta <indicasta@gmail.com>
2023-02-25 18:38:40 +0100

    adding navigation bar to project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+  document.getElementById("nav-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("navbar-bg").classList.toggle("change-bg");
+}
